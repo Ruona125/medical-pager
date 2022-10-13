f1bdd812bce8982f0cb9ef765acd73cf63694b6f
ruona <ogheneruonaagadagba4@gmail.com>
2022-10-13 13:01:04 +0100

    added the compnay header

--- client/src/components/ChannelListContainer.jsx
+++ client/src/components/ChannelListContainer.jsx
@@ -19,10 +19,20 @@ const SideBar = () => (
     </div>
   </div>
 );
+
+const CompanyHeader = () => (
+  <div className="channel-list__header">
+    <p className="channel-list__header__text">Medical Pager</p>
+  </div>
+);
+
 const ChannelListContainer = () => {
   return (
     <>
       <SideBar />
+      <div className="channel-list__list__wrapper">
+        <CompanyHeader />
+      </div>
     </>
   );
 };
